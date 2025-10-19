3484e5a24bbf74e1539f237cdfb9e7b491eb2bb8
ThanhTuan208 <nguyenthanhtuankrp1@gmail.com>
2025-10-19 23:21:26 +0700

    feat(UCKhauTru): Thêm 3 lớp DTO, DAL, BLL note: chưa đụng GUI

---
+++ DTO/DTOKhauTru.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTOKhauTru
+    {
+        public DTOKhauTru() { }
+
+        public DTOKhauTru(int id) => ID = id;
+
+        public DTOKhauTru(int id, string loaiKhauTru, decimal soTien, string moTa, string iDNguoiTao)
+        {
+            ID = id;
+            LoaiKhauTru = loaiKhauTru;
+            SoTien = soTien;
+            MoTa = moTa;
+            IDNguoiTao = iDNguoiTao;
+        }
+
+        public int ID { get; set; }
+        public string LoaiKhauTru { get; set; }
+        public decimal SoTien { get; set; }
+        public string MoTa { get; set; }
+        public string IDNguoiTao { get; set; }
+    }
+}

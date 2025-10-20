f3fab1994446fa0399b25e83b9afd15148a84d8d
ThanhTuan208 <nguyenthanhtuankrp1@gmail.com>
2025-10-20 08:24:51 +0700

    feat(UC): Thêm 3 lớp DTO, BLL, DAL cho lớp ChiTietLuong và NhanVien_PhuCap

---
+++ BLL/BLLChiTietLuong.cs
@@ -0,0 +1,132 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLChiTietLuong
+    {
+        public readonly DALChiTietLuong _dbContext;
+
+        public BLLChiTietLuong(string conn) => _dbContext = new DALChiTietLuong(conn);
+
+        // Ktra ds Chi Tiet Luong
+        public List<ChiTietLuong> KtraDsChiTietLuong()
+        {
+            try
+            {
+                if (!_dbContext.DsChiTietLuong().Any())
+                {
+                    throw new Exception("Không có dữ liệu d/s chi tiết lương !");
+                }
+                else return _dbContext.DsChiTietLuong();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi lấy d/s chi tiết lương: " + ex.Message);
+            }
+        }
+
+        // Ktra them Chi Tiet Luong
+        public bool KtraThemChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var ktraCTL = _dbContext.TimChiTietLuongQuaID(DTO.ID);
+
+                if (ktraCTL != null)
+                {
+                    var KtraThemCTL = _dbContext.ThemChiTietLuong(DTO);
+                    if (KtraThemCTL)
+                    {
+                        return true;
+                    }
+                    else throw new Exception("Lỗi thêm chi tiết lương !");
+                }
+                else throw new Exception("Không tìm thấy dữ liệu chi tiết lương !");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi thêm chi tiết lương: " + ex.Message);
+            }
+        }
+
+        // Ktra cap nhat Chi Tiet Luong 
+        public bool KtraCapNhatChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var ktraCTL = _dbContext.TimChiTietLuongQuaID(DTO.ID);
+
+                if (ktraCTL != null)
+                {
+                    var KtraThemCTL = _dbContext.CapNhatChiTietLuong(DTO);
+                    if (KtraThemCTL)
+                    {
+                        return true;
+                    }
+                    else throw new Exception("Lỗi cập nhật chi tiết lương !");
+                }
+                else throw new Exception("Không tìm thấy dữ liệu chi tiết lương trong hệ thống !");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi cập nhật chi tiết lương: " + ex.Message);
+            }
+        }
+
+        // Ktra xoa Chi Tiet Luong
+        public bool KtraXoaChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var ktraCTL = _dbContext.TimChiTietLuongQuaID(DTO.ID);
+
+                if (ktraCTL != null)
+                {
+                    var KtraThemCTL = _dbContext.XoaChiTietLuong(DTO);
+                    if (KtraThemCTL)
+                    {
+                        return true;
+                    }
+                    else throw new Exception("Lỗi xóa chi tiết lương !");
+                }
+                else throw new Exception("Không tìm thấy dữ liệu chi tiết lương trong hệ thống !");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi cập nhật chi tiết lương: " + ex.Message);
+            }
+        }
+
+        // Tim du lieu Chi Tiet Luong qua id
+        public bool KtraChiTietLuongQuaID(int id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    var ktraID = _dbContext.TimChiTietLuongQuaID(id);
+                    if (ktraID != null)
+                    {
+                        return true;
+                    }
+                    else throw new Exception($"Không tìm thấy dữ liệu chi tiết lương qua id: {id}");
+                }
+                else throw new Exception($"Kiểm tra lại dữ liệu đầu vào của id được nhập !");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi tìm id chi tiết lương: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ DAL/DALChiTietLuong.cs
@@ -0,0 +1,100 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALChiTietLuong
+    {
+        public readonly PersonnelManagementDataContextDataContext _dbContext;
+
+        public DALChiTietLuong(string conn) => _dbContext = new PersonnelManagementDataContextDataContext(conn);
+
+        // Danh sach Chi Tiet Luong
+        public List<ChiTietLuong> DsChiTietLuong() => _dbContext.ChiTietLuongs.ToList();
+
+        // Tim Chi Tiet Luong qua id
+        public ChiTietLuong TimChiTietLuongQuaID(int id) => _dbContext.ChiTietLuongs.FirstOrDefault(p => p.id == id);
+
+        // Them Chi Tiet Luong
+        public bool ThemChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var chiTietLuong = new ChiTietLuong()
+                {
+                    ngayNhanLuong = DTO.NgayNhanLuong,
+                    luongTruocKhauTru = DTO.LuongTruocKhauTru,
+                    luongSauKhauTru = DTO.LuongSauKhauTru,
+                    tongKhauTru = DTO.TongKhauTru,
+                    tongPhuCap = DTO.TongPhuCap,
+                    tongKhenThuong = DTO.TongKhenThuong,
+                    tongTienPhat = DTO.TongTienPhat,
+                    trangThai = DTO.TrangThai,
+                    ghiChu = DTO.GhiChu,
+                    idNhanVien = DTO.IDNhanVien,
+                    idKyLuong = DTO.IDKyLuong
+                };
+
+                _dbContext.ChiTietLuongs.InsertOnSubmit(chiTietLuong);
+                _dbContext.SubmitChanges();
+
+                return true;
+            }
+            catch { return false; }
+        }
+
+        // Cap nhat Chi Tiet Luong
+        public bool CapNhatChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var chiTietLuong = TimChiTietLuongQuaID(DTO.ID);
+                if (chiTietLuong != null)
+                {
+                    chiTietLuong.ngayNhanLuong = DTO.NgayNhanLuong;
+                    chiTietLuong.luongTruocKhauTru = DTO.LuongTruocKhauTru;
+                    chiTietLuong.tongKhauTru = DTO.TongKhauTru;
+                    chiTietLuong.tongPhuCap = DTO.TongPhuCap;
+                    chiTietLuong.tongKhenThuong = DTO.TongKhenThuong;
+                    chiTietLuong.tongTienPhat = DTO.TongTienPhat;
+                    chiTietLuong.trangThai = DTO.TrangThai;
+                    chiTietLuong.ghiChu = DTO.GhiChu;
+
+                    _dbContext.SubmitChanges();
+
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Xoa Chi Tiet Luong
+        public bool XoaChiTietLuong(DTOChiTietLuong DTO)
+        {
+            try
+            {
+                var chiTietLuong = TimChiTietLuongQuaID(DTO.ID);
+                if (chiTietLuong != null)
+                {
+                    _dbContext.ChiTietLuongs.DeleteOnSubmit(chiTietLuong);
+                    _dbContext.SubmitChanges();
+
+                    return true;
+                }
+                else return false;
+
+            }
+            catch { return false; }
+        }
+    }
+}
